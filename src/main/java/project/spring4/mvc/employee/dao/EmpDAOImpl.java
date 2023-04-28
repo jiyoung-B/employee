e1ae63cf7197b730371c6e7022390a7ac1a20ba1
jiyoung-B <catgirl0313@naver.com>
2023-04-28 18:40:51 +0900

    emp mybatis로

--- src/main/java/project/spring4/mvc/employee/dao/EmpDAOImpl.java
+++ src/main/java/project/spring4/mvc/employee/dao/EmpDAOImpl.java
@@ -17,92 +17,27 @@ import java.util.List;
 
 @Repository("empdao")
 public class EmpDAOImpl implements EmpDAO{
-    private static final Logger logger = LogManager.getLogger(EmpDAOImpl.class);
-//
-//    @Value("#{jdbc['selectSQL']}") private String selectSQL;
-//    @Value("#{jdbc['insertSQL']}") private String insertSQL;
-//    @Value("#{jdbc['updateSQL']}") private String updateSQL;
-//    @Value("#{jdbc['selectOneSQL']}") private String selectOneSQL;
-//    private JdbcTemplate jdbcTemplate;
-//@Autowired
-//public EmpDAOImpl(JdbcTemplate jdbcTemplate) {
-//    this.jdbcTemplate = jdbcTemplate;
-//}
+
     private SqlSession sqlSession;
     @Autowired
     public EmpDAOImpl(SqlSession sqlSession) {
-        this.sqlSession = sqlSession;
-    }
-
+        this.sqlSession = sqlSession;}
     @Override
     public int insertEmp(EmpVO emp) {
-        return sqlSession.insert("employee.insertsql", emp);
-    }
+        return sqlSession.insert("employee.insertsql", emp);}
 
     @Override
     public List<EmpVO> selectEmp() {
-//        RowMapper<EmpVO> mapper = new EmpMapper();
-//        return jdbcTemplate.query(selectSQL, mapper);
-        return sqlSession.selectList("employee.selectsql");
-    }
+        return sqlSession.selectList("employee.selectsql");}
 
     @Override
     public EmpVO selectOneEmp(int employee_id) {
-        return sqlSession.selectOne("employee.selectonesql", employee_id);
-    }
-
-    private class EmpOneMapper implements RowMapper<EmpVO> {
-        @Override
-        public EmpVO mapRow(ResultSet rs, int num) throws SQLException {
-            EmpVO emp = new EmpVO(
-                    rs.getInt(1),
-                    rs.getString(2),
-                    rs.getString(3),
-                    rs.getString(4),
-                    rs.getString(5),
-                    rs.getString(6),
-                    rs.getInt(7),
-                    rs.getString(8),
-                    rs.getInt(9),
-                    rs.getDouble(10),
-                    rs.getInt(11)
-
-            );
-            return emp;
-        }
-    }
+        return sqlSession.selectOne("employee.selectonesql", employee_id);}
 
     @Override
     public int updateEmp(EmpVO emp) {
-        //updateSQL = update employees set first_name = ?, first_name = ?, email = ?, job_id = ?, department_id = ?, \
-//        lname = ?, phone = ?, hdate = ?, sal = ?, comm = ?, mgrid = ?where employee_id = ?
-// updateSQL = update employees set job_id = ?, manager_id = ?, department_id = ? where employee_id = ?
-        Object[] param = new Object[]{
-                emp.getEmployee_id(),
-                emp.getFirst_name(),
-                emp.getLname(),
-                emp.getEmail(),
-                emp.getPhone(),
-                emp.getHdate(),
-                emp.getJob_id(),
-                emp.getSal(),
-                emp.getComm(),
-                emp.getMgrid(),
-                emp.getDepartment_id()
-        };
+        return sqlSession.update("employee.updatesql", emp);}
 
-        return jdbcTemplate.update(updateSQL, param);
-    }
-
-    private class EmpMapper implements RowMapper<EmpVO> {
-        @Override
-        public EmpVO mapRow(ResultSet rs, int num) throws SQLException {
-            EmpVO emp = new EmpVO(rs.getInt(1),rs.getString(2),
-                    rs.getString(3), rs.getString(4), rs.getInt(5));
-
-            return emp;
-        }
-    }
 
 
 
